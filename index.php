8993c25767ac679c702ad4edccdf376ff17315a3
Aikaterini Maria Gerakianaki <katerinagerak99@gmail.com>
2021-03-20 20:13:06 +0200

    small changes

--- index.php
+++ index.php
@@ -11,12 +11,12 @@
 	<link rel="stylesheet" href="assets/css/nav.css">
     <link rel="stylesheet" href="assets/css/style.css">
      <link rel="stylesheet" href="assets/css/pass.css">
-     <link rel='shortcut icon' type='image/x-icon' href='photos/uop_new_logo.png'/><meta name="description" content="UOP Logo"/>
+     <link rel='shortcut icon' type='image/x-icon' href="photos/uop_logo4_navigation.gif"/><meta name="description" content="UOP Logo"/>
     <div class="nav">
 				  <input type="checkbox" id="nav-check">
 				  <div class="nav-header">
 					<div class="nav-title">
-					 
+					 <a href="index.php"> <img src="photos/uop_logo4_navigation.gif" width="60" height="40"/> </a>
 					</div>
 				  </div>
 				  <div class="nav-btn">
@@ -52,10 +52,10 @@
 
     <div class="container">
       <label for="uname"><b>Username</b></label>
-      <input type="text" placeholder="Enter Username" name="uname" required>
+      <input type="text" placeholder="Εισάγετε Username" name="uname" required>
 
-      <label for="psw"><b>Password</b></label>
-      <input type="password" placeholder="Enter Password" name="psw" required>
+      <label for="psw"><b>Κωδικός</b></label>
+      <input type="password" placeholder="Εισάγετε κωδικό" name="psw" required>
         
       <button type="submit">Εισαγωγή</button>
      
@@ -63,7 +63,7 @@
 
     <div class="container" style="background-color:#f1f1f1">
       <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Έξοδος</button>
-      <span onclick="document.getElementById('id01').style.display='none'" class="psw"><a href="#" onclick="document.getElementById('id02').style.display='block'">Forgot password?</a></span>
+      <span onclick="document.getElementById('id01').style.display='none'" class="psw"><a href="#" onclick="document.getElementById('id02').style.display='block'">Ξεχάσατε τον κωδικό σας;</a></span>
     </div>
   </form>
 </div>
@@ -85,7 +85,7 @@
       <input  type="text" placeholder="E-mail σχολής" id="email" name="email" onblur="validateEmail(this);"  required >
       <span id='messageEmail'></span>
         
-<button onsubmit="document.getElementById('id02').style.display='none'" type="submit">Αποστολή προσωρινού κωδικού</button>
+<button onclick="document.getElementById('id02').style.display='none'" type="submit">Αποστολή προσωρινού κωδικού</button>
 
 <!--<a onsubmit="document.getElementById('id03').style.display='block'" >   </a>-->
 	  
@@ -124,7 +124,7 @@
 
 
 
-
+<script src="assets/js/emailcheck.js"></script>
 <script src="assets/js/index.js"></script>
 
 
