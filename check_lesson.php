b6e9dfde3a4bcc2a97d96047bcc20df67185d45f
Aikaterini Maria Gerakianaki <katerinagerak99@gmail.com>
2021-03-30 17:13:52 +0300

    Lesson final

--- check_lesson.php
+++ check_lesson.php
@@ -1,4 +1,8 @@
 <?php
+
+session_start();	
+
+	
 include 'config.php';
 	
 	$name=$_POST['lessonname'];
@@ -7,11 +11,27 @@ include 'config.php';
 		$sql = "INSERT INTO lesson (name, category)
 				VALUES ('$name','$category')";
         $qry = mysqli_query($conn, $sql);
+			
 
-			if($qry){
-				echo "Lesson in database!!!";
+    $idp=$_SESSION['id_professor'];
+
+   $s = mysqli_query($conn,"select id_lesson from lesson where name='$name' AND category='$category'");
+				 while ($row = mysqli_fetch_array($s, MYSQLI_ASSOC)) {
+					printf ("ID: %s ", $row["id_lesson"]);
+					 $my_lesson=$row["id_lesson"];
+				}
+				
+
+     $sqli = "INSERT INTO create_lesson (id_professor, id_lesson)
+                    VALUES ('$idp','$my_lesson')";
+
+
+        $qryi = mysqli_query($conn, $sqli);
+
+			if($qryi){
+				echo "Lesson in database final!!!";
 				// Redirecting To Other Page
-				$location="/Ptuxiaki/index.php";
+				$location="/Ptuxiaki/profilek.php";
 			header("Location: " . "http://" . $_SERVER['HTTP_HOST'] . $location);
-			}			
+			}	
 ?>
