24e5447daf40eb9952e8e3305a3a52dc67874f83
eleftheria-v <evlleafc@gmail.com>
2021-05-05 18:43:59 +0300

    Check edit

--- change_edited.php
+++ change_edited.php
@@ -11,16 +11,7 @@ $telephone=$_POST['telephone'];
 
      
 ////////////////////////
-$username = stripslashes($username);
-$email = stripslashes($email);
-$query = mysqli_query($conn,"select * from user_student where username='$username'");
-$ru = mysqli_num_rows($query);
-$query = mysqli_query($conn,"select * from user_student where email='$email'");
-$re = mysqli_num_rows($query);
-$query = mysqli_query($conn,"select * from user_professor where username='$username'");
-$rup = mysqli_num_rows($query);
-$query = mysqli_query($conn,"select * from user_professor where email='$email'");
-$rep = mysqli_num_rows($query);
+
 ////////////////////////////////////  
 		if (isset($_SESSION["id_student"])!=NULL){
 			$s = mysqli_query($conn,"select * from user_student");
@@ -28,7 +19,7 @@ $rep = mysqli_num_rows($query);
 				$mail=$row["email"];
 				$findmail=mysqli_query($conn,"select * from user_student where username='$username'");
 				while ($row = mysqli_fetch_array($findmail, MYSQLI_ASSOC)) {
-					$my_m=$row["email"];
+					$oldemail=$row["email"];
 					$tel=$row["phone_number"];
 				}
 			}
@@ -40,33 +31,53 @@ $rep = mysqli_num_rows($query);
 			    $mail=$row["email"];
 			    $findmail=mysqli_query($conn,"select * from user_professor where username='$username'");
 			    while ($row = mysqli_fetch_array($findmail, MYSQLI_ASSOC)) {
-					$my_m=$row["email"];
+					$oldemail=$row["email"];
 					$tel=$row["phone_number"];
 			    }
 			}
 			 	   				
 		}	
-						
-		if(($ru==1||$rup==1)&&($username!=$oldusername)){
+	
+	if($username!=$oldusername){//ιφ και για τα 2
+		$username = stripslashes($username);
+		$query = mysqli_query($conn,"select * from user_student where username='$username'");
+		$ru = mysqli_num_rows($query);
+		$query = mysqli_query($conn,"select * from user_professor where username='$username'");
+		$rup = mysqli_num_rows($query);
+			
+		if($ru==1||$rup==1){
 			$error = "Username taken";
 			echo "$error";
 			// Redirecting To this Page
 			$location="/Ptuxiaki/edit_prof.php?msg=failed_username";
 			header("Location: " . "http://" . $_SERVER['HTTP_HOST'] . $location);
 		}
-            else if(($re==1||$rep==1)&&($email!=$my_m)){
+	
+	}
+	else if($email!=$oldemail){
+		$email = stripslashes($email);
+		$query = mysqli_query($conn,"select * from user_student where email='$email'");
+		$re = mysqli_num_rows($query);
+		$query = mysqli_query($conn,"select * from user_professor where email='$email'");
+		$rep = mysqli_num_rows($query);
+			
+		if($re==1||$rep==1){
 				$error = "E-mail taken";
 				echo "$error";
 				// Redirecting To this Page
 				$location="/Ptuxiaki/edit_prof.php?msg=failed_mail";
 				header("Location: " . "http://" . $_SERVER['HTTP_HOST'] . $location);
 			}
-			else{
+	
+	}
+	else{
 					//$sql = "INSERT INTO user_student (name, surname, email, phone_number, AM, username,password)
 				//VALUES ('$firstname','$lastname', '$email', '$telephone','$am','$username', '$pass')";
 
 			//$qry = mysqli_query($conn, $sql);
-				echo 'Δεν γινεται ακομα αλλαγη φιλε, αλλα ειμαι σε καλο δρομο';
+				echo 'Δεν γινεται ακομα αλλαγη φιλε, αλλα ειμαι σε καλο δρομο\n';
+				echo $email, $oldemail;
+				echo $username, $oldusername;
 			/*if($qry){
 				echo "Profile changed!!";
 				// Redirecting To Other Page
@@ -75,8 +86,7 @@ $rep = mysqli_num_rows($query);
 			}*/
 			
 				
-			}
-		
+			}		
 
 
 
