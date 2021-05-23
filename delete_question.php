aa0b7fb6addc607d65bba60e6ce1fc8db9060071
eleftheria-v <evlleafc@gmail.com>
2021-05-23 16:21:13 +0300

    Delete question final

--- delete_question.php
+++ delete_question.php
@@ -10,15 +10,44 @@ $question=$_POST['txt'];
 
 //πρέπει να διαγραφούν όλες οι εγγραφές που περιέχουν το id_question
 
-$sql = "DELETE FROM question WHERE id_question=$id_question";
-	$qry = mysqli_query($conn, $sql);
-	if($qry){
+$sql1 = "DELETE FROM question WHERE id_question=$id_question";
+$qry1 = mysqli_query($conn, $sql1);
+
+	
+$sql2 = "DELETE FROM contains WHERE id_question=$id_question";
+$qry2 = mysqli_query($conn, $sql2);
+
+
+$sql3 = "DELETE FROM includes WHERE id_question=$id_question";
+$qry3 = mysqli_query($conn, $sql3);
+
+	
+$sql4 = "DELETE FROM make WHERE id_question=$id_question";
+$qry4 = mysqli_query($conn, $sql4);
+
+
+$s = mysqli_query($conn,"select * from has");
+while ($row = mysqli_fetch_array($s, MYSQLI_ASSOC)) {
+	$pa=$row["id_possibleAnswer"];
+	$findanswer=mysqli_query($conn,"select * from has where id_question='$id_question'");
+	while ($row = mysqli_fetch_array($findanswer, MYSQLI_ASSOC)) {
+		$answer=$row["id_possibleAnswer"];
+		$sql5 = "DELETE FROM possible_answer WHERE id_possibleAnswer=$answer";
+		$qry5 = mysqli_query($conn, $sql5);
+	}
+}
+
+$sql6 = "DELETE FROM has WHERE id_question=$id_question";
+$qry6 = mysqli_query($conn, $sql6);
+
+		if(($qry1)&&($qry2)&&($qry3)&&($qry4)&&($qry5)&&($qry6)){
 		$_SESSION["username"]=$username;
-		echo "Profile changed!!";
+		echo "Question deleted!!";
 		// Redirecting To Other Page
 		$location="/Ptuxiaki/select_lesson.php";
 		header("Location: " . "http://" . $_SERVER['HTTP_HOST'] . $location);
 				
-	}
+	}	
+	
 //mysqli_close($conn);
 ?>
