2d9507450065063eedbf712e95d07a08f3744460
Aikaterini Maria Gerakianaki <katerinagerak99@gmail.com>
2021-08-05 09:50:53 +0300

    mcp mc problem

--- answer.php
+++ answer.php
@@ -2,88 +2,92 @@
 include 'config.php';
 session_start ();
 $st=$_SESSION["id_student"];
-		$h = $_COOKIE["hours"];
-		$m = $_COOKIE["minutes"];
-		$s = $_COOKIE["seconds"];
-		
-		if($h<10){
-			$h="0".$h;
-		}
-		if($m<10){
-			$m="0".$m;
-		}
-		if($s<10){
-			$s="0".$s;
-		}
-		$time=$h.":".$m.":".$s;
-	
+$exam=$_SESSION["id_exam"];
 
-       
+$h = $_COOKIE["hours"];
+$m = $_COOKIE["minutes"];
+$s = $_COOKIE["seconds"];
+
+if($h<10){
+    $h="0".$h;
+}
+if($m<10){
+    $m="0".$m;
+}
+if($s<10){
+    $s="0".$s;
+}
+$time=$h.":".$m.":".$s;
+
+
+
 //mysqli_close($conn);
-	if (isset($_POST['ek'])){
+if (isset($_POST['ek'])){
 
-			$textarea=htmlspecialchars($_POST['text']);
-			$sql = "INSERT INTO answer (id_student,id_question,student_answer,time_answer)
-			VALUES ('$st', ".$_POST["ek"].",'$textarea','$time')";
+    $textarea=htmlspecialchars($_POST['text']);
+    $sql = "INSERT INTO answer (id_exam,id_student,id_question,student_answer,time_answer)
+			VALUES ('$exam','$st', ".$_POST["ek"].",'$textarea','$time')";
 
-		mysqli_query($conn,$sql);
-
-		
-		$location="/Ptuxiaki/examination2.php";
-		header("Location: " . "http://" . $_SERVER['HTTP_HOST'] . $location);	
-
-	}
-	
-	 
-	
-	if (isset($_POST['tf'])){
-		$answer=$_POST['answer'];
-		if($answer=='F'){
-			$sql = "INSERT INTO answer (id_student,id_question,student_answer,time_answer)
-			VALUES ('$st', ".$_POST["tf"].",'False','$time')";
-		}
-		if($answer=='T'){
-			$sql = "INSERT INTO answer (id_student,id_question,student_answer,time_answer)
-			VALUES ('$st', ".$_POST["tf"].",'True','$time')";
-		}
-		
-		mysqli_query($conn,$sql);
-		$location="/Ptuxiaki/examination2.php";
-		header("Location: " . "http://" . $_SERVER['HTTP_HOST'] . $location);	
-	
-	}
-
-	if (isset($_POST['mc'])){
-		$paf=$_POST['paf'];
-		$sql = "INSERT INTO answer (id_student,id_question,student_answer,time_answer)
-			VALUES ('$st', ".$_POST["mc"].",'$paf','$time')";
+    mysqli_query($conn,$sql);
 
 
-		mysqli_query($conn,$sql);
-		$location="/Ptuxiaki/examination2.php";
-		header("Location: " . "http://" . $_SERVER['HTTP_HOST'] . $location);	
-	}
+    $location="/Ptuxiaki/examination2.php";
+    header("Location: " . "http://" . $_SERVER['HTTP_HOST'] . $location);
 
- 
+}
 
-	if (isset($_POST['mcm'])){
-		$pa=$_POST['pa'];
 
-		if(!empty($_POST['pa'])) {
 
-			foreach($_POST['pa'] as $value){
-					mysqli_query($conn, "INSERT INTO answer (id_student,id_question,student_answer,time_answer)
-			VALUES ('$st', ".$_POST["mcm"].",'".$value. "','$time')");
-			}
-		}
-		
+if (isset($_POST['tf'])){
+    $answer=$_POST['answer'];
+    if($answer=='F'){
+        $sql = "INSERT INTO answer (id_exam,id_student,id_question,student_answer,time_answer)
+			VALUES ('$exam','$st', ".$_POST["tf"].",'False','$time')";
+    }
+    if($answer=='T'){
+        $sql = "INSERT INTO answer (id_exam,id_student,id_question,student_answer,time_answer)
+			VALUES ('$exam','$st', ".$_POST["tf"].",'True','$time')";
+    }
 
-			
-			$location="/Ptuxiaki/examination2.php";
-			header("Location: " . "http://" . $_SERVER['HTTP_HOST'] . $location);	
+    mysqli_query($conn,$sql);
+    $location="/Ptuxiaki/examination2.php";
+    header("Location: " . "http://" . $_SERVER['HTTP_HOST'] . $location);
 
-	}
-	
-	 
-	
+}
+
+if (isset($_POST['mc'])){
+    $paf=$_POST['paf'];
+    echo $paf;
+    $sql = "INSERT INTO answer (id_exam,id_student,id_question,student_answer,time_answer)
+			VALUES ('$exam','$st', ".$_POST["mc"].",'$paf','$time')";
+
+
+    mysqli_query($conn,$sql);
+    $location="/Ptuxiaki/examination2.php";
+    header("Location: " . "http://" . $_SERVER['HTTP_HOST'] . $location);
+}
+
+
+
+if (isset($_POST['mcm'])){
+    $pa=$_POST['pa'];
+
+    if(!empty($_POST['pa'])) {
+
+        foreach($_POST['pa'] as $value){
+           // echo "$value";
+            mysqli_query($conn, "INSERT INTO answer (id_exam,id_student,id_question,student_answer,time_answer)
+			VALUES ('$exam','$st', ".$_POST["mcm"].",'".$value. "','$time')");
+        }
+    }
+
+
+
+    $location="/Ptuxiaki/examination2.php";
+    header("Location: " . "http://" . $_SERVER['HTTP_HOST'] . $location);
+
+}
+
+
+
 ?>
--- st_exam2.php
+++ st_exam2.php
@@ -8,7 +8,7 @@ $exam=$_POST["id_exam"];
 //mysqli_close($conn);
 	if (isset($_POST["id_exam"])){
 		$queryf=mysqli_query($conn,"SELECT * FROM exam WHERE id_exam='$exam' ");
-						
+
 		while ($row = mysqli_fetch_array($queryf, MYSQLI_ASSOC)) {
 			$start_time=$row["date_time"];
 			$end_time=$row["time"];
@@ -17,18 +17,18 @@ $exam=$_POST["id_exam"];
 			$date_end = new DateTime($end_time);
 			$now = new DateTime();
 
-			if($now>$date_start) {
+			//if($now>$date_start) {
                 $_SESSION["id_exam"]=$exam;
 				$location="/Ptuxiaki/examination.php";
 				header("Location: " . "http://" . $_SERVER['HTTP_HOST'] . $location);
-			}
-			if($now<$date_start){
-                $msg="Η εξέταση σας ξεκινάει στις $start_time";
-                function_alert($msg);  
-                echo '<script type="text/JavaScript"> 
-                     history.back()
-                     </script>';
-			}
+			//}
+//			if($now<$date_start){
+//                $msg="Η εξέταση σας ξεκινάει στις $start_time";
+//                function_alert($msg);
+//                echo '<script type="text/JavaScript">
+//                     history.back()
+//                     </script>';
+//			}
 		}
 	
 		//header('Location: ' . $_SERVER['HTTP_REFERER']);
