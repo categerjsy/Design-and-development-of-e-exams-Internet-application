2ee052b2008e2753254fd9cb9f975f128993dee5
Aikaterini Maria Gerakianaki <katerinagerak99@gmail.com>
2021-08-07 15:38:28 +0300

    cor2

--- correction3.php
+++ correction3.php
@@ -7,8 +7,18 @@ $_SESSION["cor_id_student"]=$_POST["id_student"];
 $id_st=$_SESSION["cor_id_student"];
 $id_exam=$_SESSION["correction_id_exam"];
 
-$location="/Ptuxiaki/correction4.php";
-header("Location: " . "http://" . $_SERVER['HTTP_HOST'] . $location);
+$query=mysqli_query($conn,"SELECT * FROM contains WHERE id_exam='$id_exam'");
+
+while ($row = mysqli_fetch_array($query, MYSQLI_ASSOC)) {
+    $id_question=$row["id_question"];
+    $query1=mysqli_query($conn,"SELECT * FROM question WHERE id_question='$id_question' and type='True-False'");
+    while ($row1 = mysqli_fetch_array($query, MYSQLI_ASSOC)) {
+
+
+    }
+}
+//$location="/Ptuxiaki/correction4.php";
+//header("Location: " . "http://" . $_SERVER['HTTP_HOST'] . $location);
 
 
 ?>
