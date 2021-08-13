f0401f7ad292aa91e409743f6577ebd9b174a845
Aikaterini Maria Gerakianaki <katerinagerak99@gmail.com>
2021-08-13 09:57:52 +0300

    fixed time shit

--- st_exam2.php
+++ st_exam2.php
@@ -1,6 +1,8 @@
 <?php
 include 'config.php';
 session_start ();
+date_default_timezone_set('Europe/Athens') ;
+
 $exam=$_POST["id_exam"];
 
 
@@ -10,34 +12,42 @@ $exam=$_POST["id_exam"];
 		$queryf=mysqli_query($conn,"SELECT * FROM exam WHERE id_exam='$exam' ");
 
 		while ($row = mysqli_fetch_array($queryf, MYSQLI_ASSOC)) {
-			$start_time=$row["date_time"];
-			$end_time=$row["time"];
-			$_SESSION["end_exam"]=$end_time;
-			$date_start = new DateTime($start_time);
-			$date_end = new DateTime($end_time);
-			$now = new DateTime();
+			$begintime=$row["date_time"];
+			$endtime=$row["time"];
+			$_SESSION["end_exam"]=$endtime;
 
-			if(($now>=$date_start)&&($now<=$date_end)) {
-                $_SESSION["id_exam"]=$exam;
-				$location="/Ptuxiaki/examination.php";
-				header("Location: " . "http://" . $_SERVER['HTTP_HOST'] . $location);
-			}
-			else{
-                $msg="Η εξέταση σας ξεκινάει στις $start_time";
-                function_alert($msg);
-                echo '<script type="text/JavaScript">
+
+
+            $now = new Datetime();
+			$bt = new Datetime($begintime);
+			$et = new Datetime($endtime);
+
+            if($now >= $bt && $now <= $et){
+                    $_SESSION["id_exam"]=$exam;
+                    $location="/Ptuxiaki/examination.php";
+                    header("Location: " . "http://" . $_SERVER['HTTP_HOST'] . $location);
+            }
+            else{
+                    $msg="Η εξέταση σας ξεκινάει στις $begintime";
+                    function_alert($msg);
+                    echo '<script type="text/JavaScript">
                      history.back()
                      </script>';
-			}
+            }
+
+
+
 		}
 	
 		//header('Location: ' . $_SERVER['HTTP_REFERER']);
-		
+
 
 	}
     function function_alert($message) {   
            echo "<script type ='text/JavaScript'>";  
            echo "alert('$message')";  
            echo "</script>";   
-       }   
+       }
+
 ?>
+
